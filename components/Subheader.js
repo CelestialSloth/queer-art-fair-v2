e66d1f5806ce0032ed82f2285b43a3f03edd5284
Elaine Demetrion <esdemetrion@gmail.com>
2023-06-08 11:39:54 -0700

    Makes logo and Queer Art Faire text links to home

--- components/Subheader.js
+++ components/Subheader.js
@@ -1,9 +1,14 @@
+import Link from "next/link"
+
 const Subheader = (props) => {
 
     let QAFTextStyle = {
         margin: 'auto',
         textAlign: 'center',
         color: '#1A361E',
+    }
+    let linkStyle = {
+        textDecoration: 'none',
     }
 
     let smallTextStyle = {
@@ -18,7 +23,7 @@ const Subheader = (props) => {
 
     return (
         <div>
-            <h1 style={QAFTextStyle}>QUEER ART FAIRE</h1>
+            <Link style={linkStyle} href='/'><h1 style={QAFTextStyle}>QUEER ART FAIRE</h1></Link>
             <p style={smallTextStyle}>FRESH ART FAIRS IN THE BAY AREA</p>
         </div>
     )
