18f62e6f092731799689cd539aef06b84eae4161
Elaine Demetrion <esdemetrion@gmail.com>
2023-08-14 13:57:25 -0700

    Fixes label import console error by changing {Label} to Label

--- app/shops/new/ShopForm.jsx
+++ app/shops/new/ShopForm.jsx
@@ -4,7 +4,7 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Input from "../../components/forms/input";
 import styles from "./ShopForm.module.css";
 import { useRouter } from "next/navigation";
-import { Label } from "../../components/forms/label";
+import Label from "../../components/forms/label";
 // the form to create a new shop
 // Info needed: owner_id, name, description, items (just initialize the array)
 
